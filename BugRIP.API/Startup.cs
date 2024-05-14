c668d5c5eee179663966a1f2fb8fc6684362f3fe
daily-dev-jake <jlele92@outlook.com>
2024-05-14 19:42:12 +0800

    Set up bugs endpoint

---
+++ BugRIP.API/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugRIP.API
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+
+		}
+	}
+}
